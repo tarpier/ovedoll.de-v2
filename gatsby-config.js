ac371720c3419a0ce3852920a4ea48cfbe481115
Ove Doll <ove.doll@aboutyou.de>
2018-09-11 14:37:25 +0200

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,7 +105,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`,
+        icon: `src/assets/ove-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
